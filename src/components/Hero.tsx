4c566f16a91f85cbff73ec1c4af87fef5e4025b8
kitkovsky <okitkowski114@gmail.com>
2022-03-07 22:40:00 +0100

    feat: hero content and basic styling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,62 @@
 import React from "react";
+import styled from "styled-components";
+import heroImage from "../assets/hero-img.svg";
+import { PrimaryColours } from "../styles/GlobalStyles";
+
+const Section = styled.section`
+  margin: 7rem 5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @media screen and (max-width: 750px) {
+    flex-direction: column;
+  }
+`;
+
+const HeroText = styled.div`
+  flex: 1 1 20rem;
+  margin-right: 2rem;
+
+  .hero-name {
+    color: ${PrimaryColours.blue}
+  }
+`;
+
+const HeroMainText = styled.h1`
+  font-weight: bold;
+`;
+
+const HeroImg = styled.div`
+  flex: 1 1 20rem;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  img {
+    max-width: 90%;
+  }
+
+  @media screen and (max-width: 750px) {
+    justify-content: center;
+  }
+`;
 
 const Hero: React.FC = () => {
   return (
-    <div>
-      <h1>this is the hero</h1>
-    </div>
+    <Section id="hero">
+      <HeroText>
+        <p className="hero-name">Oliwer Kitkowski</p>
+        <HeroMainText>Nowoczesne strony internetowe</HeroMainText>
+        <p>
+          Tworzę nowoczesne, wysokowydajne i responsywne strony internetowe z
+          wykorzystaniem najnowszych technologii.
+        </p>
+      </HeroText>
+      <HeroImg>
+        <img src={heroImage} alt="hero image" />
+      </HeroImg>
+    </Section>
   );
 };
 
