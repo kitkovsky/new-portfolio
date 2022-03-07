b5efb262ad0c78fc42ba6f1ba6eef38c58f475a5
kitkovsky <okitkowski114@gmail.com>
2022-03-07 21:44:04 +0100

    feat: basic nav layout and styling

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,61 @@
 import React from "react";
+import styled from "styled-components";
+import logo from "../assets/logo.svg";
 
-const About: React.FC = () => {
+const Header = styled.header`
+  display: flex;
+  max-height: 10rem;
+  margin: 0rem 5rem;
+  position: sticky;
+  top: 0;
+  backdrop-filter: blur(20px);
+`;
+
+const Logo = styled.a`
+  flex-grow: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+
+  img {
+    height: 45%;
+  }
+`;
+
+const Ul = styled.ul`
+  display: flex;
+  flex-direction: row;
+  flex-grow: 3;
+  align-items: center;
+  justify-content: space-around;
+
+  li {
+    list-style: none;
+  }
+
+  @media screen and (max-width: 950px) {
+    flex-grow: 20;
+  }
+
+  @media screen and (max-width: 750px) {
+    background: red;
+  }
+`;
+
+const Nav: React.FC = () => {
   return (
-    <div>
-      <h1>this is the nav</h1>
-    </div>
+    <Header>
+      <Logo href="index.html">
+        <img src={logo} alt="logo" />
+      </Logo>
+      <Ul>
+        <li><a href="#">Strona Główna</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#tools">Umiejętności</a></li>
+        <li><a href="#contact">Kontakt</a></li>
+      </Ul>
+    </Header>
   );
 };
 
-export default About;
+export default Nav;
