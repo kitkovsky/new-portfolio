97c851a35f3ca95c5e2f56b4fc80a7213b3ddfba
kitkovsky <okitkowski114@gmail.com>
2022-03-07 19:57:43 +0100

    feat: testing if netlify builds correctly with images

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { GlobalStyle } from "./styles/GlobalStyles";
+import logo from "./assets/logo.svg";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <GlobalStyle />
       <h1>well hello there</h1>
+      <img src={logo} alt="logo" />
     </div>
   );
 }
