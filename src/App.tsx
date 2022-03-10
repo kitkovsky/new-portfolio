e21d8baa9d1a0fc23286664c35505a2d6ea83884
kitkovsky <okitkowski114@gmail.com>
2022-03-10 16:40:45 +0100

    fix: netlify please?

--- src/App.tsx
+++ src/App.tsx
@@ -114,7 +114,7 @@ const App: React.FC = () => {
         }
       }
     }
-  }, [heroInView, portfolioInView, toolsInView, contactInView]);
+  }, [sectionStates, sectionsInView]);
 
   return (
     <div className="App">
